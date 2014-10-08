552fd5d633c31e1290ed3a82b96ce5584fee4ffe
Keika Jones <keika.jones@gmail.com>
2014-10-08 11:31:45 -0400

    created migrations and models

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+	has_many :posts
+	has_many :relationships, foreign_key: :follower_id, dependent: :destroy
+	has_many :followeds, through: :relationships
+	has_many :reverse_relationships, foreign_key: :followed_id, class_name: "Relationship", dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
+	validates_presence_of :fname, :lname, :email, :password
+end
