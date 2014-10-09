7f6fbafad53025bc223abc606740e98cf1ef53e5
Keika Jones <keika.jones@gmail.com>
2014-10-09 17:38:44 -0400

    added display_tag method to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ def current_user
 	session[:user_id] ? User.find(session[:user_id]) : nil	
 end
 
+def display_tag
+		tags = [' ','Salad', 'Soup', 'Seafood', 'Meat/Poultry', 'Dessert']
+		tags[self.tag.to_i]
+end
+
 end
