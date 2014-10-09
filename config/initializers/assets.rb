c26bd3e6adba2ea20526b228fee37c56ad8693ae
Darlene Portades <darleneportades@gmail.com>
2014-10-09 00:14:19 -0400

    Landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,11 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( component.css )
+Rails.application.config.assets.precompile += %w( demo.css )
+Rails.application.config.assets.precompile += %w( cbpScroller.js )
+Rails.application.config.assets.precompile += %w( modernizr.custom.js )
+Rails.application.config.assets.precompile += %w( classie.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
