4d2983e1c579e32b87ec1b5293fd534b3c1e0177
Keika Jones <keika.jones@gmail.com>
2014-10-10 12:19:29 -0400

    edited form for Edit User

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,7 +46,7 @@ class PostsController < ApplicationController
 
 	private
 	def post_params
-		params.require(:post).permit(:body, :tag).merge(user_id: current_user.id)
+		params.require(:post).permit(:title, :body, :tag).merge(user_id: current_user.id)
 	end
 
 	def set_post
