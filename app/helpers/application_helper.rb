70f42550fe3b0a88a166622ba3e27ed1b769956c
Keika Jones <keika.jones@gmail.com>
2014-10-14 17:32:10 -0400

    Tag URL implimented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,25 @@ module ApplicationHelper
 	end
 
 	def display_tag
-			tags = [' ','Salad', 'Soup', 'Seafood', 'Meat/Poultry', 'Dessert']
-			tags[self.tag.to_i]
+		tags = [' ','Salad', 'Soup', 'Seafood', 'Meat/Poultry', 'Dessert']
+		tags[self.tag.to_i]
+	end
+
+	def return_post_path(post)
+		case post.tag
+		when "1"
+			salads_path
+		when "2"
+			soups_path
+		when "3"
+			seafood_path
+		when "4"
+			meat_path
+		when "5"
+			desserts_path
+		else
+			homepage_path
+		end
 	end
 
 end
